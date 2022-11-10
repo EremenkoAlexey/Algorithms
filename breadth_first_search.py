3749415d1402d32e2b88e73c4e6b45e15245da34
Alexey Eremenko <EremenkoAlexey@users.noreply.github.com>
2022-11-10 13:46:44 +0300

    Create breadth_first_search.py

---
+++ breadth_first_search.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:41:49 2022
+
+@author: User
+"""
+initGraph = {
+    "A": ['B', 'C'],
+    "B": ['D', 'E'],
+    "C": ['F', 'G'],
+    "D": [],
+    "E": ['H'],
+    "F": [],
+    "G": [],
+    "H": []}
+      #      A
+      #    /   \
+      #   B     C
+      #  /  \   / \
+      # D    E F   G
+      #       \
+      #        H
+ROOT = 'A'
+KEY = 'H'
+visited = {ROOT:""}
+
+def breadth_first_search(graph, root, key)->bool:
+    """Looks for KEY in graph. Returns True if KEY found"""
+    queue = [root]
+    def check_cell(value: str)-> str:
+        if value == key:
+            return True
+        for cell in graph[value]:
+            queue.append(cell)
+            visited[cell] = value
+        return False
+    while True:
+        if len(queue) > 0:
+            if check_cell(queue.pop(0)):
+                return True
+        else:
+            return False
+
+def push(value: str)->str:
+    """Prints the way to the KEY"""
+    cur_value = visited[value]
+    if cur_value != "":
+        return push(cur_value)+"->"+value
+    return value
+
+if breadth_first_search(initGraph, ROOT, KEY):
+    print("Route to", KEY, "is", push(KEY))
+else:
+    print("Key "+ KEY+ " doesn't exist!")
+    
